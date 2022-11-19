b8817211ac7ed0ae8e9b089fa50004a657a99ab6
Guglielmo Faglioni <guglielmo@gorillatech.io>
2022-11-19 12:05:43 +0100

    Update spm-check-updates.rb

--- Formula/spm-check-updates.rb
+++ Formula/spm-check-updates.rb
@@ -1,8 +1,8 @@
 class SpmCheckUpdates < Formula
   desc "Check for SPM updates"
   homepage "https://github.com/gorillatech/spm-check-updates"
-  url "https://github.com/gorillatech/spm-check-updates/archive/refs/tags/0.1.1.zip"
-  sha256 "37d1905e1380239d82cc64ef3925862516b52b3c39f2a773e2406fc502103f4f"
+  url "https://github.com/gorillatech/spm-check-updates/archive/refs/tags/0.1.2.zip"
+  sha256 "d8b6dc52da5fcb3daf4d99c6f10d4a9e0213ecccc2fa75cff54acb954b272468"
   license "MIT"
   
   def install
