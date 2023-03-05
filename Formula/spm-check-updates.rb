540cbde16889c2df1279b9629ea4dff9d0a40b6b
Guglielmo Faglioni <guglielmo@gorillatech.io>
2023-03-05 01:31:34 +0100

    Update spm-check-updates.rb

--- Formula/spm-check-updates.rb
+++ Formula/spm-check-updates.rb
@@ -1,8 +1,8 @@
 class SpmCheckUpdates < Formula
-  desc "Check for SPM updates"
+  desc "spm-check-updates shows if there are new versions available for SPM (Swift Package Manager) dependencies used in a Xcode project on in another Package.swift."
   homepage "https://github.com/gorillatech/spm-check-updates"
-  url "https://github.com/gorillatech/spm-check-updates/archive/refs/tags/0.1.3.zip"
-  sha256 "fc06d0e901d1df656d9745084821a2e609348db151d8b108a4cd696407f5b561"
+  url "https://github.com/gorillatech/spm-check-updates/archive/refs/tags/0.1.4.zip"
+  sha256 "af38ff74902581fa87005a55964aa762c3ad5f88b3371df91f1fd6f97ee1f438"
   license "MIT"
   
   def install
